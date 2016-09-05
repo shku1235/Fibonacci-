1979f5669eb90e34a834731a5e3f19934ad57545
shku135 <shku1235@gmail.com>
2016-09-05 11:07:42 +0530

    G

--- views.py
+++ views.py
@@ -11,18 +11,16 @@ def index(request):
 def fibview(request):
     print "here"
     res=None
-    # if this is a POST request we need to process the form data
+   
     if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
+       
         form = fibform(request.POST)
         print 'got post'
         print form
         print form.cleaned_data
         # check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
+           
 
             print form.cleaned_data
             que=form.cleaned_data
